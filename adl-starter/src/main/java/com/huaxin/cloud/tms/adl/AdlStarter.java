d59e27e91b01a5e2191e46c405c4343781247040
liutianci <liutianci@huaxincem.com>
2020-12-29 18:03:26 +0800

    项目结构+依赖关系

---
+++ adl-starter/src/main/java/com/huaxin/cloud/tms/adl/AdlStarter.java
@@ -0,0 +1,19 @@
+package com.huaxin.cloud.tms.adl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *     adl启动类
+ * </p>
+ *
+ * @author LiuTianci
+ * @date 2020-12-29 14:22
+ */
+@SpringBootApplication
+public class AdlStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AdlStarter.class, args);
+    }
+}
